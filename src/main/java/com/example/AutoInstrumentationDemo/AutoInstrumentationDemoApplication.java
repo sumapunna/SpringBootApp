e6086a67602e22ab06c5bac892fbf37f6d44ffd9
Suma Punna <sumapunna@Sumas-MacBook-Air.local>
2025-01-25 18:26:07 +0530

    updated code

---
+++ src/main/java/com/example/AutoInstrumentationDemo/AutoInstrumentationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutoInstrumentationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInstrumentationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoInstrumentationDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/AutoInstrumentationDemo/DiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.AutoInstrumentationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DiceApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+}
+
